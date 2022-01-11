902bdc58c373fef55f23e4fc22c4cbff15664156
WDW <boa3465@gmail.com>
2022-01-12 00:33:23 +0900

    예제 작성 마무리

--- src/python/2.struct_echo.py
+++ src/python/2.struct_echo.py
@@ -21,18 +21,33 @@ class Packet(Structure):
 MY_DATA_TYPE = 1
 data = Packet(ord('#'), MY_DATA_TYPE, 123, 456)
 
+# 데이터 출력
+print(f"start_sign : {data.start_sign}\
+        data_type : {data.data_type}\
+        data : {data.data}\
+        checksum : {data.checksum}")
+
 # 바이트로 변환해서 출력
-print(bytes(data))
+print(f"bytes : {bytes(data)}")
 
 arduino = serial.Serial(port = "COM6", baudrate = 9600)
 time.sleep(3)
 
 # 시리얼에 데이터를 보냄
-arduino.write(bytes(data))
-# 잠시 대기
-time.sleep(1)
-data = arduino.read_all()
-print(data)
+send_count = arduino.write(bytes(data))
+
+# 보낸 사이즈만큼 읽어들임
+data = arduino.read(send_count)
+
+# 받은 bytes형식의 데이터를 구조체로 변환
+data = Packet.from_buffer_copy(data)
+
+# 데이터 출력
+print(f"start_sign : {data.start_sign}\
+        data_type : {data.data_type}\
+        data : {data.data}\
+        checksum : {data.checksum}")
+# 바이트로 변환해서 출력
+print(f"bytes : {bytes(data)}")
 
 arduino.close()
-
